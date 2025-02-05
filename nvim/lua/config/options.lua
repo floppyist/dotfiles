da12d4f72bd8676925d2285ac1f2c0d771cfb78b
floppyist <floppyism@gmail.com>
2025-02-05 17:09:23 +0100

    Reworking nvim without nvim-java plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Use telescope as default
+vim.g.lazyvim_picker = "telescope" 
+
+-- Replace tabs with space
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
