5c9d101ac657d8f9b236aaa0ddd1bf9a99555c1b
floppyist <floppyism@gmail.com>
2026-02-18 15:18:35 +0000

    Removed iconsin dunst config; Adjusted notification width and colors

--- scripts/bluetooth-toggle.sh
+++ scripts/bluetooth-toggle.sh
@@ -3,9 +3,9 @@
 if rfkill list bluetooth | grep -q "yes"; then
     rfkill unblock bluetooth
     bluetoothctl power on
-    notify-send "Bluetooth" "on" -i bluetooth-on
+    notify-send "Bluetooth" "on"
 else
     bluetoothctl power off
     rfkill block bluetooth
-    notify-send "Bluetooth" "off" -i bluetooth-off
+    notify-send "Bluetooth" "off"
 fi
